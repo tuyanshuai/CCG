251ad5fea169f5e4f0277c37fc231506ef330240
C Wen <whywe@live.com>
2018-09-16 18:06:11 -0400

    update shortest_path

--- common/shortest_path.m
+++ common/shortest_path.m
@@ -1,14 +1,12 @@
-function path = shortestpath(mesh,cc)
-nv = mesh.nv;
+function path = shortest_path(mesh,cc)
 vert = mesh.vert;
 edge = mesh.edge;
 de = vert(edge(:,1),:)-vert(edge(:,2),:);
 el = sqrt(dot(de,de,2));
-G = sparse(edge(:,1),edge(:,2),el,nv,nv);
-G = G+G';
+G = graph([edge(:,1);edge(:,2)],[edge(:,2);edge(:,1)],[el;el]);
 path = [];
 for i = 2:length(cc)
-    [~,pathi] = graphshortestpath(G,cc(i-1),cc(i));
+    pathi = shortestpath(G,cc(i-1),cc(i));
     path = [path,pathi(1:end-1)];
 end
 path = [path,cc(end)];
